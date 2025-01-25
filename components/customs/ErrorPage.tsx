f4975415663ddb9e0d3c65073a95e3c7a69a5949
Mohammad Zaid Shaikh <zaidshaikhhulk@gmail.com>
2025-01-25 23:31:04 +0530

    Remove unused frontend files and configurations

--- components/customs/ErrorPage.tsx
+++
@@ -1,9 +0,0 @@
-interface ErrorMessageProps {
-    message: string;
-}
-
-export const ErrorMessage = ({ message }: ErrorMessageProps) => (
-    <div className="min-h-[400px] flex items-center justify-center text-red-500">
-        {message}
-    </div>
-);
